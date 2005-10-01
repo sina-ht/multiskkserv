20b6d5b6869a020c1435b471e4b264628c171e50
Hiroshi Takekawa <sian@fennel.org>
2005-10-01 01:44:28 +0000

    More sophisticated autogen.sh.

--- autogen.sh
+++ autogen.sh
@@ -1,12 +1,64 @@
-#! /bin/sh
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
 
-aclocal
-autoheader
-automake --gnu -a -c Makefile
-for i in `find . -name Makefile.am`; do
-  f=`echo $i | sed 's/.am//'`
-  automake $f
-done
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=. 
+
+THEDIR=`pwd`
+cd $srcdir
+DIE=0
+
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have autoconf installed to compile multiskkserv."
+	echo "Download the appropriate package for your distribution,"
+	echo "or see http://www.gnu.org/software/autoconf"
+	DIE=1
+}
+
+(libtool --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have libtool installed to compile multiskkserv."
+	echo "Download the appropriate package for your distribution,"
+	echo "or see http://www.gnu.org/software/libtool"
+	DIE=1
+}
+
+(automake --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	DIE=1
+	echo "You must have automake installed to compile multiskkserv."
+	echo "Download the appropriate package for your distribution,"
+	echo "or see http://www.gnu.org/software/automake"
+}
+
+if test "$DIE" -eq 1; then
+	exit 1
+fi
+
+test -f src/multiskkserv.c || {
+	echo "You must run this script in the top-level multiskkserv directory"
+	exit 1
+}
+
+if test -z "$*"; then
+	echo "I am going to run ./configure with no arguments - if you wish "
+        echo "to pass any to it, please specify them on the $0 command line."
+fi
+
+libtoolize --copy --force
+aclocal $ACLOCAL_FLAGS
+automake --add-missing
 autoconf
 
-./configure $*
+cd $THEDIR
+
+if test x$OBJ_DIR != x; then
+    mkdir -p "$OBJ_DIR"
+    cd "$OBJ_DIR"
+fi
+
+$srcdir/configure "$@"
+
+echo 
+echo "Now type 'make' to compile multiskkserv."

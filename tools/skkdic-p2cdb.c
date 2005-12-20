fea561f61ea23d8a90e3d30564a7edade2b4a68b
Hiroshi Takekawa <sian@fennel.org>
2005-12-20 15:27:51 +0000

    Use SKKSERV_WORD_SIZE, SKKSERV_RESULT_SIZE of multiskkserv.h.  Unlink .cdb file first.  Use err_message().

--- tools/skkdic-p2cdb.c
+++ tools/skkdic-p2cdb.c
@@ -1,9 +1,9 @@
 /*
  * skkdic-p2cdb.c -- convert plain skkdic to cdb.
- * (C)Copyright 2001 by Hiroshi Takekawa
+ * (C)Copyright 2001-2005 by Hiroshi Takekawa
  * This file is part of multiskkserv.
  *
- * Last Modified: Sun Feb 24 01:33:08 2002.
+ * Last Modified: Tue Dec 20 23:17:15 2005.
  * $Id$
  *
  * This software is free software; you can redistribute it and/or
@@ -24,6 +24,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <errno.h>
 
 #include <cdb_make.h>
 #include <cdb.h>
@@ -33,8 +34,7 @@
 #include "compat.h"
 #include "common.h"
 
-#define SKKSERV_WORD_SIZE 1023
-#define SKKSERV_RESULT_SIZE 2048
+#include "src/multiskkserv.h"
 #define LINE_SIZE (SKKSERV_WORD_SIZE + SKKSERV_RESULT_SIZE)
 
 int
@@ -49,17 +49,26 @@ main(int argc, char **argv)
   char rbuf[SKKSERV_RESULT_SIZE];
 
   if (argc != 2) {
-    fprintf(stderr, "usage: %s outfile < infile\n", argv[0]);
+    printf("skkdic-p2cdb version " VERSION "\n");
+    printf("(C)Copyright 2001-2005 by Hiroshi Takekawa\n\n");
+    printf("usage: %s outfile < infile\n", argv[0]);
     return 1;
   }
 
+  if (unlink(argv[1])) {
+    if (errno != ENOENT) {
+      perror(argv[0]);
+      return 12;
+    }
+  }
+
   if ((fd = open(argv[1], O_CREAT | O_WRONLY | O_EXCL, S_IRUSR)) == -1) {
-    fprintf(stderr, "Cannot open %s\n", argv[1]);
+    err_message("Cannot open %s\n", argv[1]);
     return 2;
   }
 
   if (cdb_make_start(&cdb, fd) == -1) {
-    fprintf(stderr, "cdb_make_start() failed.\n");
+    err_message("cdb_make_start() failed.\n");
     return 3;
   }
 
@@ -68,43 +77,50 @@ main(int argc, char **argv)
       continue;
     if (buf[0] == '\n')
       continue;
+
     if ((sep = strchr(buf, ' ')) == NULL) {
-      fprintf(stderr, "format error: %s\n", buf);
+      err_message("format error: %s\n", buf);
       return 4;
     }
     memcpy(wbuf, buf, sep - buf);
     wbuf[sep - buf] = '\0';
+
+    if (strlen(sep + 1) > SKKSERV_RESULT_SIZE) {
+      err_message("too long entry, increase SKKSERV_RESULT_SIZE (%d -> %d): %s\n", SKKSERV_RESULT_SIZE, strlen(sep + 1), wbuf);
+      return 11;
+    }
     strcpy(rbuf, sep + 1);
     /* chomp */
     if (rbuf[strlen(rbuf) - 1] == '\n')
       rbuf[strlen(rbuf) - 1] = '\0';
+
     if (cdb_make_addbegin(&cdb, strlen(wbuf), strlen(rbuf)) == -1) {
-      fprintf(stderr, "cdb_make_addbegin() failed.\n");
+      err_message("cdb_make_addbegin() failed.\n");
       return 5;
     }
     if (buffer_puts(&cdb.b, wbuf) == -1) {
-      fprintf(stderr, "buffer_puts() failed.\n");
+      err_message("buffer_puts() failed.\n");
       return 6;
     }
     h = CDB_HASHSTART;
     for (i = 0; i < strlen(wbuf); i++)
       h = cdb_hashadd(h, wbuf[i]);
     if (buffer_puts(&cdb.b, rbuf) == -1) {
-      fprintf(stderr, "buffer_puts() failed.\n");
+      err_message("buffer_puts() failed.\n");
       return 7;
     }
     if (cdb_make_addend(&cdb, strlen(wbuf), strlen(rbuf), h) == -1) {
-      fprintf(stderr, "cdb_make_addend() failed.\n");
+      err_message("cdb_make_addend() failed.\n");
       return 8;
     }
   }
 
   if (cdb_make_finish(&cdb) == -1) {
-    fprintf(stderr, "cdb_make_finish() failed.\n");
+    err_message("cdb_make_finish() failed.\n");
     return 9;
   }
   if (close(fd) == -1) {
-    fprintf(stderr, "final close() failed...\n");
+    err_message("final close() failed...\n");
     return 10;
   }
 
